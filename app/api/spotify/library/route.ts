2de74c455693da2267b5fc0ca7663c027dd5d8f7
Theodore Locke <locketheodore27@gmail.com>
2026-07-10 14:04:42 +0100

    Fix TypeScript type error in Spotify library route

--- app/api/spotify/library/route.ts
+++ app/api/spotify/library/route.ts
@@ -13,7 +13,7 @@ async function fetchAllSavedTracks(accessToken: string): Promise<SpotifyTrack[]>
   let url: string | null = 'https://api.spotify.com/v1/me/tracks?limit=50'
 
   while (url) {
-    const res = await fetch(url, {
+    const res: Response = await fetch(url, {
       headers: { Authorization: `Bearer ${accessToken}` },
     })
 
